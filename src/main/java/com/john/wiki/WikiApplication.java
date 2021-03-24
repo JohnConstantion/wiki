b6e4f6cd0e0c28fda3934eac13ed0909d392c65f
constantinejohn <1906172933@qq.com>
2021-03-24 15:15:54 +0800

    Initial commit

---
+++ src/main/java/com/john/wiki/WikiApplication.java
@@ -0,0 +1,18 @@
+package com.john.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author constantinejohn
+ */
+@SpringBootApplication
+public class WikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+
+}
