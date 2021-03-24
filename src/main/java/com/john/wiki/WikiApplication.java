d5bddfbd32c51c311130864043b93806172ddc83
constantinejohn <1906172933@qq.com>
2021-03-24 15:47:07 +0800

    1.1.0 修改端口号；增加启动成功的日志

--- src/main/java/com/john/wiki/WikiApplication.java
+++ src/main/java/com/john/wiki/WikiApplication.java
@@ -1,7 +1,10 @@
 package com.john.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * 启动类
@@ -11,8 +14,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+
+        // 添加启动成功的日志
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功！！！");
+        LOG.info("地址：\thttps://127.0.0.1:{}", environment.getProperty("server.port"));
     }
 
 }
