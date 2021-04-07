9f5d08bf6ad0b76b754b61455bd929af734218db
constantinejohn <1906172933@qq.com>
2021-04-07 21:34:20 +0800

    5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/john/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,25 @@
+package com.john.wiki.config;
+
+import com.john.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * 拦截器
+ *
+ * @author constantinejohn
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+    }
+}
